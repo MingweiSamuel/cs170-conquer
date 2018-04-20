c28301bf8858eb0dbcd3d096c177f1104ca9f982
Mingwei Samuel <mingwei.samuel@gmail.com>
2018-04-20 08:24:48 +0000

    adding GLKH

--- glns_interface.py
+++ glns_interface.py
@@ -16,7 +16,7 @@ def run(dist, ids, clusters, timeout=10):
     """
     G: gtsp graph (complete).
     """
-    path = 'GLNS/inputs/temp_interface_{}_{}.txt'.format(os.getpid(), random.randint(0, int(1e9)))
+    path = 'GLNS/inputs/temp_interface_{}_{}.gtsp'.format(os.getpid(), random.randint(0, int(1e9)))
     with open(path, 'w+') as output_file:
         gtsp.output_gtsp(output_file, dist, ids, clusters, name='temp_interface')
     print('  Wrote {}. Running GLNS with timeout {}.'.format(path, timeout))
